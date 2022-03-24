96a8af8d64d4cf77d406078a3706ea4930b591e3
vinnie0454 <97425789+vinnie0454@users.noreply.github.com>
2022-03-24 13:24:08 +0800

    Create TeacherUITable.php
    change the fields if possible

---
+++ TeacherUITable.php
@@ -0,0 +1,32 @@
+<div class="content">
+	<div class="container-fluid">
+		<div>   
+			<a href="#"><strong><span class="ti-crown"></span> Teachers Section</strong></a>
+			<hr>		
+			<div class="panel-heading">
+				<div class="row">
+					<div class="col-md-10">
+						<h3 class="panel-title"></h3>
+					</div>
+					<div class="col-md-2" align="right">
+						<button type="button" name="add" id="addTeacher" class="btn btn-success btn-xs">Add New Teacher</button>
+					</div>
+				</div>
+			</div>
+			<table id="teacherList" class="table table-bordered table-striped">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Name</th>	
+						<th>Assigned Subjects</th>	
+						<th>Class</th>	
+						<th>Sections</th>								
+						<th></th>
+						<th></th>							
+					</tr>
+				</thead>
+			</table>
+			
+		</div>			
+	</div>		
+</div>	
